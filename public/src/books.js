50d37e5d3bf0008070e9faa94d72cf560dac0e0e
andrew fritz <andrew.fritz86@gmail.com>
2015-07-25 15:31:15 -0400

    first commit

---
+++ public/src/books.js
@@ -0,0 +1,33 @@
+var DemoDiv = React.createClass({
+  render: function(){
+    return (
+
+        <div className="bralf">
+          Sup Man
+        </div>
+      );
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+React.render(<DemoDiv />, document.getElementById("main"));
